ba6f15717585a1a4e0042ec22fe1a14d0e333eab
Conner Stephen McCabe <conner@systemicconfluence.com>
2018-08-14 22:48:33 +0100

    Adding Simple Toy Robot Base Class and Base RSpec Test
    - Also added Gemfile for bundle installations

---
+++ spec/toy_robot_spec.rb
@@ -0,0 +1,9 @@
+require "./lib/ToyRobot.rb"
+
+describe "ToyRobot" do
+    describe ".new" do
+        it "should create a new instance of ToyRobot" do
+            toy_robot = ToyRobot.new 
+        end
+    end
+end
