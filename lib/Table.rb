fdca29fb2be9ad1669080c824baaa35c06d1b462
Conner Stephen McCabe <conner@systemicconfluence.com>
2018-08-15 00:12:01 +0100

    Adding New Table Class and Method Placeholders for ToyRobot class

---
+++ lib/Table.rb
@@ -0,0 +1,5 @@
+class Table
+    def initialize(table_grid_x, table_grid_y)
+        
+    end
+end
--- lib/ToyRobot.rb
+++ lib/ToyRobot.rb
@@ -1,4 +1,22 @@
 class ToyRobot
     def initialize()
+        table = Array.new(5)
     end
+    
+    # Function to take a command and execute a run sequence
+    def read_command(command)
+    end
+    
+    # Function to move the robot 1 square in the direction it's currently facing
+    def move
+    end
+
+    # Function to take an X and Y co-ordinate to place the robot at along with a resulting direction to face in
+    def place(x, y, direction_to_face)
+    end
+    
+    # Function to report the robot's current X and Y co-ordinates and the direction it's facing
+    def report
+    end
+
 end
