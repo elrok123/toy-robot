ebc8e85e39be5a6faa2aa000bab8c9ef1ed4f88d
Conner Stephen McCabe <conner@systemicconfluence.com>
2018-08-15 01:50:06 +0100

    Adding Some Logic to the ToyRobot class and the Table Class
    - Added some logic to the ToyRobot's parse_command_string method for breaking up a command string 
        + Will branch to appropriate methods based on commands found
    - Added some logic to the

--- lib/Table.rb
+++ lib/Table.rb
@@ -1,5 +1,21 @@
 class Table
     def initialize(table_grid_x, table_grid_y)
-        
+       @max_x = table_grid_x
+       @max_y = table_grid_y
+    end
+
+    # Function to check if a destination is a valid place on the table
+    def valid_destination(x_position, y_position)
+        if x_position > 0 && x_position < @max_x
+            if y_position > 0 && y_position < @max_y
+                return true  
+            else
+                puts "Invalid Y position provided..."
+                retrun false
+            end
+        else
+            puts "Invalid X position provided..."
+            return false
+        end
     end
 end
--- lib/ToyRobot.rb
+++ lib/ToyRobot.rb
@@ -1,10 +1,33 @@
+require "./lib/Table"
+
 class ToyRobot
     def initialize()
-        table = Array.new(5)
+        @current_position = [nil, nil]
+        @table = Table.new(5, 5)
     end
     
+    # Function to parse commands from an input source
+    def parse_command_string(command_string)
+        # Split our command into segments to be further parsed
+        command_segments = command_string.split(" ")
+
+        command_segments.each_with_index do |command_segment, i|
+            case command_segment.to_sym
+            when :MOVE
+                this.move
+            when :PLACE
+                x_position = command_segments[i+1]
+                y_position = command_segments[i+2]
+
+                this.place x_position, y_position 
+            when :REPORT
+            end
+        end
+    end
+
     # Function to take a command and execute a run sequence
     def read_command(command)
+        
     end
     
     # Function to move the robot 1 square in the direction it's currently facing
@@ -13,6 +36,9 @@ class ToyRobot
 
     # Function to take an X and Y co-ordinate to place the robot at along with a resulting direction to face in
     def place(x, y, direction_to_face)
+        if 
+            return true
+            return false
     end
     
     # Function to report the robot's current X and Y co-ordinates and the direction it's facing
